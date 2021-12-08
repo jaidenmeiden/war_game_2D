c23b83ca0f9f23980705984483218737e0585cec
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-08 09:47:30 +0100

    feat: Add attributes practice

--- Assets/Scripts/Attributes.cs
+++ Assets/Scripts/Attributes.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Attributes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Space] // Include a vertical space
+    [Header("Numerical values")] // Put a title to variables group
+    public int score;
+    [SerializeField] // Allows to display private variables in the editor and modify them
+    private int money;
+
+    [Space] 
+    [Header("Others values")] 
+    [SerializeField]
+    private string myName;
+    [HideInInspector] // Allows to hide public variables in the editor
+    public int range;
+    
+    [Space] 
+    [Header("Tools")] 
+    [Range(0,5)] // Assign slide to variable
+    public int playerLifes;
+
+    [Min(0)] // Assign minimum value
+    public int height;
+    
+    [Space]
+    [Header("Texts")]
+    public string ocupation;
+    [TextArea] // Assign textArea to variable
+    public string description;
+    
+    [Header("Assignation")] 
+    [Tooltip("This is the player size")] 
+    [SerializeField]
+    private string size;
+
+    // Put a submenu option to call the function in the component menu assigned to this script
+    [ContextMenu("Call MyFunction")] 
+    public void MyFunction()
     {
-        
+        Debug.Log("MyFunction is working!");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Player player;
+}
+
+[System.Serializable]
+public class Player
+{
+    public string name;
+    public string breed;
+    public int age;
 }
