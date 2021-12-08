165c4385e5c23e25fcdc66cc6a2e59c29cb3535f
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-08 21:15:53 +0100

    feat: Finished fade nad callbacks implementation

--- Assets/Scripts/Transition/Fade.cs
+++ Assets/Scripts/Transition/Fade.cs
@@ -16,12 +16,23 @@ public class Fade : MonoBehaviour
     [ContextMenu("FadeIn")]
     public void FadeIn()
     {
-        spriteRenderer.DOFade(1,2);
+        spriteRenderer.DOFade(1,2).OnComplete(() =>
+        {
+            Debug.Log("FadeIn finished!");
+        });
     }
     
     [ContextMenu("FadeOut")]
     public void FadeOut()
     {
-        spriteRenderer.DOFade(0,2);
+        spriteRenderer.DOFade(0,2).OnComplete(StartGame).OnStart(() =>
+        {
+            Debug.Log("FadeOut started!");
+        });
+    }
+
+    private static void StartGame()
+    {
+        Debug.Log("FadeOut finished!");
     }
 }
