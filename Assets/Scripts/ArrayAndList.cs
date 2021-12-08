1f8cec3bb60dc9da7e817db540b1300e9a6d4b92
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-08 10:04:13 +0100

    feat: Add Arrays and List practice

---
+++ Assets/Scripts/ArrayAndList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayAndList : MonoBehaviour
+{
+    public int[] numbers = new int[2];
+    
+    public int[] count;
+
+    public List<string> names = new List<string>();
+    private void Start()
+    {
+        foreach (int item in count)
+        {
+            Debug.Log(item);
+        }
+        
+        names.Add("Jaiden");
+        names.Add("Juaquin");
+        names.Add("Jeronimo");
+        names.Remove("Jeronimo");
+        
+        foreach (string item in names)
+        {
+            Debug.Log(item);
+        }
+    }
+}
