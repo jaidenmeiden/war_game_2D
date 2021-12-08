98cd98143358c648f6d1c09b8eac71d5b4ce8c09
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-09 00:15:46 +0100

    feat: Add player life script

---
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    public GameObject explosionEffect;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            //FindObjectOfType<GameManager>().PlayerKilled();
+            
+            /*GameObject go = Instantiate(explosionEffect);
+            go.transform.position = transform.position;
+            Destroy(gameObject);*/
+        }
+    }
+}
