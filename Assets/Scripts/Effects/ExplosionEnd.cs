785cf2e6e33c04663e73788e300dadaeacf2533b
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-08 13:57:34 +0100

    feat: Adjust explosion

--- Assets/Scripts/Effects/ExplosionEnd.cs
+++ Assets/Scripts/Effects/ExplosionEnd.cs
@@ -2,22 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExplotionEnd : MonoBehaviour
+public class ExplosionEnd : MonoBehaviour
 {
     public void EndExplosion()
     {
         Destroy(gameObject);
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
