6cb1420f7979bd23513eda885b70de0635f8cf91
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-08 13:47:29 +0100

    feat: Create explosion script effect

---
+++ Assets/Scripts/Effects/ExplosionEnd.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplotionEnd : MonoBehaviour
+{
+    public void EndExplosion()
+    {
+        Destroy(gameObject);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
