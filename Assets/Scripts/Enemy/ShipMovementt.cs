5293b0b02f2d25f9f7fbae8e3567fa6087faaf65
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-08 15:48:16 +0100

    feat: Add ship movement script

---
+++ Assets/Scripts/Enemy/ShipMovementt.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShipMovementt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMoveX(0, 2);
+    }
+}
