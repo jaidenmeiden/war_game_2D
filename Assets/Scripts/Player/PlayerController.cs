45cf81506a90898008c9107c5403b898e8bb2375
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-08 12:26:09 +0100

    feat: Add player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Add Rigidbody2D to the player automatically
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movementX, movementY) * speed;
+    }
+}
